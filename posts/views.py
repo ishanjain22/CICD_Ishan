c85d5a5c77a97022f62e4bdddaaba4218a279833
Ishan Jain <ishanjain22@gmail.com>
2019-04-25 18:03:42 -0700

    First commit

---
+++ posts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+
+
+class PostPageView(ListView):
+    model = Post
+    template_name = 'posts.html'
+# Create your views here.
